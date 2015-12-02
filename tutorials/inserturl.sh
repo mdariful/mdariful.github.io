8101af1724a29fb7967b1ced1f9db2306242ee1d
mdariful <md.ariful@email.com>
2015-12-03 00:00:02 +0100

    aggiunto ora e sistemato link allo script

--- tutorials/inserturl.sh
+++ tutorials/inserturl.sh
@@ -3,5 +3,6 @@ echo -e "Inserire un link da aggiungere file indice\n";
 read ilmiolink;
 echo -e "Ora inserire il nome da dare al link\n";
 read nomelink;
-sed -i '6i\'"<li><a href="$ilmiolink">"$nomelink"</a></li>" index.html
+NOW=$(date +"%d-%m-%Y-%r")
+sed -i '6i\'"<li><a href="\""$ilmiolink""\">""$nomelink""</a> -- <span> aggiunto il $NOW</span></li>" index.html
 echo -e "ho aggiunto il link\n";
