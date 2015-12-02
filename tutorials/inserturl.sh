b1b25f5b3c5f7e28f10db4b1b2cb975810806861
mdariful <md.ariful@email.com>
2015-12-02 22:53:26 +0100

    script migliorato ora il link viene aggiunto dopo una riga fissa

--- tutorials/inserturl.sh
+++ tutorials/inserturl.sh
@@ -3,5 +3,5 @@ echo -e "Inserire un link da aggiungere file indice\n";
 read ilmiolink;
 echo -e "Ora inserire il nome da dare al link\n";
 read nomelink;
-echo -e "<li><a href="$ilmiolink">"$nomelink"</a></li>">>index.html;
+sed -i '6i\'"<li><a href="$ilmiolink">"$nomelink"</a></li>" index.html
 echo -e "ho aggiunto il link\n";
